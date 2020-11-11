e2fbd5ff28f32acb6a99fbdef448bfab0d452cef
aditya Kharade <adityakharade51@gmail.com>
2020-11-11 16:29:01 +0530

    Added: added code for doing operations on 5 random numbers

---
+++ TwoDigitOperations.js
@@ -0,0 +1,9 @@
+let val1 = Math.floor(Math.random() * 90) + 10;
+let val2 = Math.floor(Math.random() * 90) + 10;
+let val3 = Math.floor(Math.random() * 90) + 10;
+let val4 = Math.floor(Math.random() * 90) + 10;
+let val5 = Math.floor(Math.random() * 90) + 10;
+
+let sumOfVal = val1+val2+val3+val4+val5;
+let avg = sumOfVal / 5;
+console.log("Sum of 5 no is: "+sumOfVal+" And Avg is: "+avg);
