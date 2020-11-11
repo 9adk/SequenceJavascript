34721ce653312d00dced10712b64c5211970ea98
aditya Kharade <adityakharade51@gmail.com>
2020-11-11 16:30:06 +0530

    Added: added code for unit conversion

---
+++ UnitConversion.js
@@ -0,0 +1,5 @@
+console.log("Conversion of 42 inches in fit is: "+42/12);
+let plotArea = (60 * 40)*0.3048;
+console.log("Rectangular plot of 60x40 feet in meters is: "+(plotArea));
+let plotInMeters = plotArea * 0.000247105;
+console.log("Area of 25 such plots in acres" + 25 * plotInMeters);
